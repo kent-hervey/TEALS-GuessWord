12cab99ad4b2bd28a5ff19e124c824dd35ad0571
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-05-11 14:27:11 -0500

    completed testing of readDictionary

--- src/MayClassTester.java
+++ src/MayClassTester.java
@@ -1,9 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MayClassTester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		
+		String filename = "hangman.txt";
+		
+		//testing readDictionary to see if it reads all words from input file of words
+		String[] dictionary = HangmanUtility.readDictionary(new Scanner(new File(filename)));
+		
+		//testing dictionary by seeing output
+		HangmanUtility.printFirst10Array(dictionary);
+		
+		
+		String foundWord = HangmanUtility.getRandomWord();
 		
 		
 		
