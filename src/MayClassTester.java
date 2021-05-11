2cd0afe9711987aacde1f53341b5432cc512ef8d
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-05-11 14:00:04 -0500

    MayClassTester for testing utility hangman classes

---
+++ src/MayClassTester.java
@@ -0,0 +1,24 @@
+
+public class MayClassTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
